2fc2901c31299f6eaa5928b95286332261e92028
Quterma <wolf921@mail.ru>
2023-06-09 17:57:43 +0300

    add history

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Loader } from "./components/loader";
 import { Tabs } from "./components/tabs";
 import { Overview } from "./components/overview";
 import { inPercents } from "./helpers";
+import { History } from "./components/history";
 
 export enum ETabs {
 	Installs = "Installs",
@@ -15,12 +16,12 @@ export type Row = {
 	country: string;
 	[ETabs.Installs]: number;
 	[ETabs.ROI]: number;
-	industry_roi: number | undefined;
+	industry_roi: number;
 };
 
 function App() {
 	const [activeTab, setActiveTab] = useState<string>(ETabs.Installs);
-	const [countries, setCountries] = useState<InstallsData[] | null>(null);
+	const [countries, setCountries] = useState<InstallsData[]>([]);
 	const [companies, setCompanies] = useState<Company[]>([]);
 	const [isLoading, setIsLoading] = useState<boolean>(true);
 	const [isError, setIsError] = useState<boolean>(false);
@@ -36,48 +37,40 @@ function App() {
 			.finally(() => setIsLoading(false));
 	}, []);
 
-	const getRows = (companies: Company[]): Row[] =>
-		companies
-			.map(({ display_name, companyData }) =>
-				companyData.map(({ country, installs, revenue, cost, iso }) => {
-					const currentCountry = countries?.find(country => country.iso === iso);
-					const industry_roi = currentCountry && inPercents(currentCountry.revenue / currentCountry.cost);
+	const rows: Row[] = companies
+		.map(({ display_name, companyData }) =>
+			companyData.map(({ country, installs, revenue, cost, iso }) => {
+				const currentCountry = countries.find(country => country.iso === iso);
+				const industry_roi = currentCountry ? inPercents(currentCountry.revenue / currentCountry.cost) : 0;
 
-					return {
-						display_name,
-						country,
-						[ETabs.Installs]: installs,
-						[ETabs.ROI]: inPercents(revenue / cost),
-						industry_roi,
-					};
-				})
-			)
-			.flat();
+				return {
+					display_name,
+					country,
+					[ETabs.Installs]: installs,
+					[ETabs.ROI]: inPercents(revenue / cost),
+					industry_roi,
+				};
+			})
+		)
+		.flat();
 
 	// const filterRows = (rows: Row[]) => (({ field, value }: {field: string; value: string | number}) => {
 
 	// }
+	console.log(rows);
 
 	const tabConfig = Object.entries(ETabs).reduce((acc, [key, title]) => {
-		const overviewData = getRows(companies)
-			.slice(0, 5)
-			.map(company => ({
-				name: company.display_name,
-				country: company.country,
-				data: company[key as keyof typeof ETabs],
-			}));
+		const overviewData = rows.slice(0, 5).map(company => ({
+			name: company.display_name,
+			country: company.country,
+			data: company[key as keyof typeof ETabs],
+		}));
 
 		return {
 			...acc,
 			[key]: {
 				title,
-				component: isError ? (
-					<div className="error">Something went wrong!</div>
-				) : isLoading ? (
-					<Loader />
-				) : (
-					<Overview companies={overviewData} dataType={title} />
-				),
+				component: <Overview companies={overviewData} dataType={title} />,
 			},
 		};
 	}, {});
@@ -85,9 +78,15 @@ function App() {
 	return (
 		<div className="main-wrapper">
 			<div className="main-header">Data Reports</div>
-			<Tabs activeTab={activeTab} setActiveTab={setActiveTab} config={tabConfig}>
-				<div className="">some content</div>
-			</Tabs>
+			{isError ? (
+				<div className="error">Something went wrong!</div>
+			) : isLoading ? (
+				<Loader />
+			) : (
+				<Tabs activeTab={activeTab} setActiveTab={setActiveTab} config={tabConfig}>
+					<History rows={rows} />
+				</Tabs>
+			)}
 		</div>
 	);
 }
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,1 +1,3 @@
 export const inPercents = (num: number) => Math.round(num * 10000) / 100;
+
+export const toFixed = (num: number, places = 2) => Number(num).toFixed(places);
